d770447d908e42bc89efe9844e4f36d95bcadaf6
CodeDDAGGI <qudrb1132@naver.com>
2025-01-24 21:38:01 +0900

    Feat : "시큐리티"

--- security/src/main/java/com/test/security/config/Security.java
+++ security/src/main/java/com/test/security/config/Security.java
@@ -2,8 +2,10 @@ package com.test.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.HeadersDsl;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +14,17 @@ public class Security {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
-                .csrf(csrfConfig
-                    -> csrfConfig.disable())
-                .headers(headers ->headers.frameOptions(frameOptionsConfig ->
-                        frameOptionsConfig.disable()));
-
+                .csrf(AbstractHttpConfigurer::disable) // CSRF(Cross-Site Request Forgery) 보호를 비활성화
+                .headers(AbstractHttpConfigurer::disable) // 기본 보안 헤더를 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // 폼 기반 로그인을 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) // HTTP 기본 인증을 비활성화
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 관리 정책을 무상태(Stateless)로 설정
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth  -> auth
+                        .requestMatchers("/api/public/**").permitAll()
+                        .anyRequest().authenticated()
+                );
 
         return httpSecurity.build();
     }
